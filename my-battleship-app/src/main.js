6662c9f5d3eb1e8b220fb85362fd50b1a6db7c32
Viteail <vitea.sirbu1@gmail.com>
2024-06-11 12:14:25 +0300

    feat: add handleBoardClick

--- my-battleship-app/src/main.js
+++ my-battleship-app/src/main.js
@@ -1,8 +1,25 @@
 import "./styles.css";
 import { createPageLayout } from "./modules/layout";
 import { Player } from "./modules/player";
+import { handleBoardClick } from "./modules/events";
 
-createPageLayout();
+const startGame = () => {
+  const player = new Player();
+  const computer = new Player();
 
-const player = new Player();
-const computer = new Player();
+  computer.gameboard.placeShip(computer.gameboard.ships[0], {
+    col: 0,
+    row: 0,
+    vertical: true,
+    orizontal: false,
+  });
+
+  createPageLayout();
+
+  const enemyBoard = document.querySelector(".pc-board");
+  enemyBoard.addEventListener("click", (e) =>
+    handleBoardClick(e, enemyBoard, computer),
+  );
+};
+
+startGame();
--- my-battleship-app/src/modules/events.js
+++ my-battleship-app/src/modules/events.js
@@ -1,3 +1,20 @@
-export const handleBoardClick = (e) => {
-  console.log(e);
+export const handleBoardClick = (e, board, computer) => {
+  const elements = Array.from(board.children);
+  const elementIndex = elements.indexOf(e.target);
+
+  const coords = {
+    col: isNaN(Number(("" + elementIndex)[1]))
+      ? elementIndex
+      : Number(("" + elementIndex)[1]),
+    row: isNaN(Number(("" + elementIndex)[1]))
+      ? 0
+      : Number(("" + elementIndex)[0]),
+  };
+
+  computer.gameboard.receiveAttack(coords);
+  if (computer.gameboard.board[coords.col][coords.row] === "x")
+    elements[elementIndex].classList.add("bg-red-400");
+  else elements[elementIndex].classList.add("bg-sky-400");
+  console.log(coords.col, coords.row);
+  console.log(computer.gameboard.board[coords.col][coords.row]);
 };
