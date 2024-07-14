1a519cb07933ca1e1cea0658d2ec74998a67b4d8
Viteail <vitea.sirbu1@gmail.com>
2024-07-14 12:34:05 +0300

    fix: min width player turn

--- my-battleship-app/src/modules/UI/playerTurn.js
+++ my-battleship-app/src/modules/UI/playerTurn.js
@@ -1,5 +1,5 @@
 export const createCurrentPlayerTurn = () =>
-  `<div id='player-turn' class='text-center max-w-[400px] p-10 text-3xl'>Player's Turn!</div>`;
+  `<div id='player-turn' class='text-center min-w-[400px] p-10 text-3xl'>Player's Turn!</div>`;
 
 export const updateCurrentPlayerTurn = (player) => {
   const playerTurn = document.querySelector("#player-turn");
